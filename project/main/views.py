c400dec36302c15acdc36a5a1f0c6dd454b1073e
Galimatiasz <mateuszgajewski2233553@gmail.com>
2022-07-09 15:05:20 +0200

    Optymalizacja kodu.

--- project/main/views.py
+++ project/main/views.py
@@ -5,11 +5,6 @@ import requests
 def index(response):
     data = requests.get(
         'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=ethereum%2C%20bitcoin%2C%20cardano&order=market_cap_desc&per_page=100&page=1&sparkline=false').json()
-    processedData = []
-    context = {}
-
-    for cryptoCurrency in data:
-        processedData.append(cryptoCurrency)
-        
-    context = {'currencies': processedData}
+     
+    context = {'currencies': data}
     return render(response, 'main/page.html', context)
